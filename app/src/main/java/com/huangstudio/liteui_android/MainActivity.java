e832a6127623e8fe19174f8f2adf7aced07c9504
David <justforjobonly@126.com>
2019-07-04 17:32:51 +0800

    完成lite dialog的修改

--- app/src/main/java/com/huangstudio/liteui_android/MainActivity.java
+++ app/src/main/java/com/huangstudio/liteui_android/MainActivity.java
@@ -1,6 +1,7 @@
 package com.huangstudio.liteui_android;
 
 import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,18 +12,28 @@ import android.widget.Toast;
 
 
 import com.huangstudio.liteui.LSButton;
+import com.huangstudio.liteui.dialog.SweetAlertDialog;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "MainActivity";
     LSButton mLSButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mLSButton = findViewById(R.id.switch_button);
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+        findViewById(R.id.button5).setOnClickListener(this);
+        TestLBSwitch();
+    }
+
+    // 异步switch
+    private void TestLBSwitch(){
         mLSButton.setEnableToggleWait(true);
         mLSButton.setOnToggleWaitListener(new LSButton.ToggleWaitListener() {
             @Override
@@ -55,23 +66,42 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void test(View v) {
-        final ValueAnimator animator = ValueAnimator.ofInt(0, 100);
-        animator.setDuration(5000);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                /**
-                 * 通过这样一个监听事件，我们就可以获取
-                 * 到ValueAnimator每一步所产生的值。
-                 *
-                 * 通过调用getAnimatedValue()获取到每个时间因子所产生的Value。
-                 * */
-                Integer value = (Integer) animation.getAnimatedValue();
-                ((Button)findViewById(R.id.button)).setText(value + "");
-            }
-        });
-        animator.start();
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.button://普通dialog
+
+                break;
+            case R.id.button2://警告dialog
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Are you sure?")
+                        .setContentText("Won't be able to recover this file!")
+                        .setConfirmText("Yes,delete it!")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog
+                                        .setTitleText("Deleted!")
+                                        .setContentText("Your imaginary file has been deleted!")
+                                        .setConfirmText("OK")
+                                        .setConfirmClickListener(null)
+                                        .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                            }
+                        })
+                        .show();
+                break;
+            case R.id.button3://禁止dialog
+                break;
+            case R.id.button4://等待dialog
+                final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                pDialog.setTitleText("Loading");
+                pDialog.setCancelable(true);
+                pDialog.show();
+                break;
+            case R.id.button5://自定义dialog
+                break;
+        }
     }
 
 }
